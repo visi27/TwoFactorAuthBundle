b980eb3ce5a6a0aed400225b2631f4134989ab55
Evis Bregu <evis.bregu@gmail.com>
2017-07-04 17:00:33 +0200

    Adding encryption key and logger to Encryption Service

--- Security/Encryption/DummyEncryptionService.php
+++ Security/Encryption/DummyEncryptionService.php
@@ -9,9 +9,31 @@
 namespace Evis\TwoFactorAuthBundle\Security\Encryption;
 
 
+use Psr\Log\LoggerInterface;
+
 class DummyEncryptionService implements EncryptionServiceInterface
 {
+    /**
+     * @var string
+     */
+    private $key;
+    /**
+     * @var LoggerInterface
+     */
+    private $logger;
 
+    /**
+     * EncryptionService constructor.
+     *
+     * @param string          $key
+     * @param LoggerInterface $logger
+     */
+    public function __construct($key, LoggerInterface $logger)
+    {
+
+        $this->key = $key;
+        $this->logger = $logger;
+    }
     /**
      * @param $plainText
      *
